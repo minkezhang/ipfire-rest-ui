abcd4b096ef23cc888d8c34ad459563426a4c121
Minke Zhang <minke.zhang@gmail.com>
2019-07-14 01:27:41 +0000

    Add simple table renderer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FooComponent } from './components/foo/foo.component'
+import { FooComponent } from './components/foo/foo.component';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatTableModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+import { SshSessionsComponent } from './components/ssh-sessions/ssh-sessions.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooComponent
+    FooComponent,
+    SshSessionsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
