07405a327f671782cf35ee40463d51dc421dc1a5
Minke Zhang <minke.zhang@gmail.com>
2019-07-22 09:26:25 +0000

    Temporarily render mobile view on all viewports

--- src/app/directives/media-layout/media-layout.directive.ts
+++ src/app/directives/media-layout/media-layout.directive.ts
@@ -16,7 +16,11 @@ export class MediaLayoutDirective implements OnInit {
   public is_large: Observable<BreakpointState>;
 
   constructor(protected breakpoint: BreakpointObserver) {
-    this.is_small = breakpoint.observe([Breakpoints.Handset]);
+    this.is_small = breakpoint.observe([
+        Breakpoints.Handset,
+        Breakpoints.Tablet,
+        Breakpoints.Web]);
+    // this.is_small = breakpoint.observe([Breakpoints.Handset]);
     this.is_large = breakpoint.observe([Breakpoints.Tablet, Breakpoints.Web]);
   }
 
